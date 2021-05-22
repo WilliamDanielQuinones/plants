b0d3751447003841851aec0d4c730e316654f4cf
Daniel Quinones <williamdanielquinones@gmail.com>
2021-05-22 16:39:34 -0400

    base config

---
+++ src/views/App/App.js
@@ -0,0 +1,17 @@
+import logo from '../../assets/plant.svg';
+import './styles.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Welcome to the plant zone
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
