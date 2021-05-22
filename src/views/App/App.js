42519450e380999abed44cbbc34c080c906e08ce
Daniel Quinones <williamdanielquinones@gmail.com>
2021-05-22 17:49:33 -0400

    add theming

--- src/views/App/App.js
+++ src/views/App/App.js
@@ -1,14 +1,23 @@
+/** @jsxImportSource theme-ui */
+import React from 'react';
+import { Button } from 'rebass';
 import logo from '../../assets/plant.svg';
 import './styles.scss';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" 
+      sx={{
+        fontWeight: 'bold',
+        fontSize: 4, // picks up value from `theme.fontSizes[4]`
+        backgroundColor: 'primary', // picks up value from `theme.colors.primary`
+      }}>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Welcome to the plant zone
         </p>
+        <Button sx={{variant: 'buttons.primary'}}>Push</Button>
       </header>
     </div>
   );
