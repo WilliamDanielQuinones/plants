c0c64953a70784527905b12afd4968d2a56e3831
DanielQuinones <45363476+WilliamDanielQuinones@users.noreply.github.com>
2021-05-28 17:43:36 -0400

    deploy again

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket         = "daniel-personal-terraform-state"
+    bucket         = "daniels-personal-terraform-state"
     key            = "plants/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
