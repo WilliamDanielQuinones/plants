327d6397847f6a8e66a3aa7938e3d0df25b6a2c1
DanielQuinones <45363476+WilliamDanielQuinones@users.noreply.github.com>
2021-05-28 17:06:58 -0400

    terraform fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,7 +7,7 @@ terraform {
   backend "s3" {
     bucket         = "daniel-personal-terraform-state"
     key            = "plants/terraform.tfstate"
-    region         = "us-east-1"
+    region         = "us-east-2"
     encrypt        = true
   }
 }
